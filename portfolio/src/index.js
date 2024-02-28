58f0108e1837bd26dec04f7625203efce6b8e11a
Nicolas Corbett <nicolas.corbett@student.ehb.be>
2024-02-28 18:00:05 +0100

    createdRouteMain

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Main from './components/Main';
+import Main from './routes/Main';
+//import Main from './components/Main';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {path: '/', 
+   element: <Main /> 
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
